2c8688edae146b295795d10de64ace8d7dc47fd5
ka.herrerah <ka.herrerah@duocuc.cl>
2026-05-09 02:19:53 -0400

    FRONTEND

--- backend/innovatech/src/main/java/com/backend/innovatech/InnovatechApplication.java
+++ backend/innovatech/src/main/java/com/backend/innovatech/InnovatechApplication.java
@@ -1,13 +1,13 @@
 package com.backend.innovatech;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Importa esto
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // Añade la exclusión aquí
 public class InnovatechApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InnovatechApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InnovatechApplication.class, args);
+    }
 }
